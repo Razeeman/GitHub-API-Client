e38e6ecc1bccc4787497b05b3b7aff5c2256d5bc
Razeeman <freeraz@mail.ru>
2019-05-08 11:57:29 +0300

    Refactored.

--- app/src/main/java/com/razeeman/showcase/githubapi/data/api/ApiService.kt
+++ app/src/main/java/com/razeeman/showcase/githubapi/data/api/ApiService.kt
@@ -1,6 +1,6 @@
 package com.razeeman.showcase.githubapi.data.api
 
-import com.razeeman.showcase.githubapi.data.api.model.SearchResponse
+import com.razeeman.showcase.githubapi.data.api.model.RepositorySearchResponse
 import io.reactivex.Observable
 import retrofit2.http.GET
 import retrofit2.http.Query
@@ -8,6 +8,6 @@ import retrofit2.http.Query
 interface ApiService {
 
     @GET("search/repositories")
-    fun findRepositories(@Query("q") query: String): Observable<SearchResponse>
+    fun findRepositories(@Query("q") query: String): Observable<RepositorySearchResponse>
 
 }
--- app/src/main/java/com/razeeman/showcase/githubapi/data/api/model/RepositorySearchResponse.kt
+++ app/src/main/java/com/razeeman/showcase/githubapi/data/api/model/RepositorySearchResponse.kt
@@ -5,7 +5,7 @@ import com.google.gson.annotations.SerializedName
 /**
  * Data class for converting Json response to search query.
  */
-data class SearchResponse (
+data class RepositorySearchResponse (
 
     @SerializedName("total_count") val total_count : Int,
     @SerializedName("incomplete_results") val incomplete_results : Boolean,
