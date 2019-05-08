f199eb72b6c7f5695436b0bc38ccf5fefffdcf8d
Razeeman <freeraz@mail.ru>
2019-05-08 11:37:59 +0300

    Switched to RxJava.

--- app/src/main/java/com/razeeman/showcase/githubapi/MainActivity.kt
+++ app/src/main/java/com/razeeman/showcase/githubapi/MainActivity.kt
@@ -1,17 +1,21 @@
 package com.razeeman.showcase.githubapi
 
-import androidx.appcompat.app.AppCompatActivity
+import android.annotation.SuppressLint
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import com.razeeman.showcase.githubapi.data.api.ApiService
-import com.razeeman.showcase.githubapi.data.api.model.SearchResponse
+import com.razeeman.showcase.githubapi.data.api.model.Repository
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.CompositeDisposable
+import io.reactivex.schedulers.Schedulers
 import kotlinx.android.synthetic.main.activity_main.*
-import retrofit2.Call
-import retrofit2.Callback
-import retrofit2.Response
-import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.Retrofit
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
+import retrofit2.converter.gson.GsonConverterFactory
 
 class MainActivity : AppCompatActivity() {
+
+    private val compositeDisposable = CompositeDisposable()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,19 +27,34 @@ class MainActivity : AppCompatActivity() {
         val retrofit = Retrofit.Builder()
             .baseUrl(baseUrl)
             .addConverterFactory(GsonConverterFactory.create())
+            .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
             .build()
         val apiService = retrofit.create(ApiService::class.java)
-        val call = apiService.findRepositories(baseQuery)
 
-        call.enqueue(object : Callback<SearchResponse> {
-            override fun onFailure(call: Call<SearchResponse>, t: Throwable) {
-                response_text.text = t.message
-            }
-            override fun onResponse(call: Call<SearchResponse>, response: Response<SearchResponse>) {
-                for (repository in response.body()!!.items) {
-                    response_text.text = "${response_text.text}${repository.name}\n"
-                }
-            }
-        })
+        val disposable = apiService.findRepositories(baseQuery)
+            .subscribeOn(Schedulers.computation())
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribe(
+                { updateView(it.items) },
+                { showError(it.message) }
+            )
+
+        compositeDisposable.add(disposable)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        compositeDisposable.dispose()
+    }
+
+    @SuppressLint("SetTextI18n")
+    private fun updateView(repos: List<Repository>) {
+        for (repository in repos) {
+            response_text.text = "${response_text.text}${repository.name}\n"
+        }
+    }
+
+    private fun showError(message: String?) {
+        response_text.text = message
     }
 }
--- app/src/main/java/com/razeeman/showcase/githubapi/data/api/ApiService.kt
+++ app/src/main/java/com/razeeman/showcase/githubapi/data/api/ApiService.kt
@@ -1,13 +1,13 @@
 package com.razeeman.showcase.githubapi.data.api
 
 import com.razeeman.showcase.githubapi.data.api.model.SearchResponse
-import retrofit2.Call
+import io.reactivex.Observable
 import retrofit2.http.GET
 import retrofit2.http.Query
 
 interface ApiService {
 
     @GET("search/repositories")
-    fun findRepositories(@Query("q") query: String): Call<SearchResponse>
+    fun findRepositories(@Query("q") query: String): Observable<SearchResponse>
 
 }
